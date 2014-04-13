07e1e4ae33b35fa1eaeafd2a29736c68c809199d
j0sanc02 <j0sanc02@louisville.edu>
2014-04-13 12:45:31 -0400

    Final Project Commot2

---
+++ CarLot2/CarLot2/Models/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarLot2.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+
+        [Range(1900, 2100)]
+        public int Year { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string Make { get; set; }
+
+        [StringLength(60, MinimumLength = 1)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string Model { get; set; }
+
+        [Range(1, 1000000)]
+        public int Mileage { get; set; }
+
+        [StringLength(30, MinimumLength = 1)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string ExtColor { get; set; }
+
+        [StringLength(30, MinimumLength = 1)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        public string IntColor { get; set; }
+
+
+        [Range(1, 2000000)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+
+    }
+
+    public class CarDBContext:DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
